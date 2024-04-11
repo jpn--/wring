b6e6e936ade1b727212afff5deba9681d8b7419c
Jeff Newman <jeff@driftless.xyz>
2024-04-11 16:44:34 -0500

    add omx to wring

--- src/wring/__init__.py
+++ src/wring/__init__.py
@@ -5,3 +5,4 @@ from ._version import __version__, __version_tuple__  # noqa
 from ._app import app  # noqa
 from .tar_zst import tarzst, untarzst  # noqa
 from .csv import csv  # noqa
+from .omx import convert_multiple_omx  # noqa
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import random
 import string
 from pathlib import Path
+import numpy as np
 
 import pytest
 
@@ -16,3 +17,17 @@ def junk_file(tmp_path_factory) -> Path:
     ]
     file.write_text("\n".join(content * 1000))
     return file
+
+
+@pytest.fixture(scope="session")
+def junk_omx(tmp_path_factory) -> Path:
+    """Create a junk OMX file."""
+    file = tmp_path_factory.mktemp("data") / "junk.omx"
+    from wring.omx import OMX
+    prng = np.random.default_rng(42)
+    with OMX(file, "w") as omx:
+        omx.add_matrix("A", prng.uniform(size=(100, 100)))
+        omx.add_matrix("B", prng.normal(size=(100, 100)).astype(np.float32))
+        omx.add_lookup("TAZ", np.arange(1, 101, dtype=np.int8))
+    return file
+
